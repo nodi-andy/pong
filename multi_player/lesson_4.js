b71f613c0b3b1ec741414f102844c99469cfca7f
Andy <andy@doganmail.de>
2021-12-27 02:17:51 +0100

    multiplayer completed

--- multi_player/lesson_4.js
+++ multi_player/lesson_4.js
@@ -1,19 +1,14 @@
 const { randomUUID } = require('crypto');
 const express = require('express');
 const app = express();
+app.use(express.static('public'));
 const users = new Map();
 require('express-ws')(app);
-let player1             = {x: 200, y: 700, len:  50 , dir: 0,  point: 0};
-let player2             = {x: 200, y: 100, len:  50 , dir: 0,  point: 0};
-
-app.use(express.static('public'));
 app.listen(80);
 
 app.ws('/', function(ws, req) {
   ws.uuid = randomUUID();
   users.set(ws.uuid, ws);
-  if (users.size == 1) ws.player = player1;
-  if (users.size == 2) ws.player = player2;
   ws.on('message', function(message) {
     console.log(message);
   });
--- multi_player/lesson_5.js
+++ multi_player/lesson_5.js
@@ -37,29 +37,6 @@ render();
 function render(){ 
    player1.x   = player1.x + player1.dir;
    player2.x   = player2.x + player2.dir;
-   ball.x      = ball.x + ball.vx;
-   ball.y      = ball.y + ball.vy;
-   if (ball.x < 0 || ball.x > 600) ball.vx = -ball.vx;
-   if (ball.y < player2.y) {
-      if (ball.x < player2.x - player2.len || ball.x > player2.x + player2.len) {
-          ball = {x:   300      , y:    300     , vx:   1               , vy: -5};
-          player2.point = 0;
-      } else {
-          ball.vx = ball.vx + (ball.x - player2.x) * 0.1;
-          ball.vy = ball.vy * -1.01;
-          player2.point++;
-      }
-  }
-  if (ball.y > player1.y) {
-      if (ball.x < player1.x - player1.len || ball.x > player1.x + player1.len) {
-          ball = {x:   300     , y:    300     , vx:   1               , vy: -5};
-          player1.point = 0;
-      } else {
-          ball.vx = ball.vx + (ball.x - player1.x) * 0.1;
-          ball.vy = ball.vy * -1.01;
-          player1.point++;
-      }
-  }
   setTimeout(render, 15);
   sendAll(JSON.stringify({msg:"update", data: {ball, player1, player2}}));
 }
